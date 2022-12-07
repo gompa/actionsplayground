b6ed99210f0dbed22c8ea755f86af4459b03de35
gompa <gompa@h-bomb.nl>
2022-12-07 20:10:00 +0100

    versionfiles

--- kversion.py
+++ kversion.py
@@ -7,6 +7,11 @@ print(githubworkspace)
 repoversionfile="version"
 sourcedebversionfile="{githubworkspace}/linux/.version".format(githubworkspace=githubworkspace)
 kernelrevisionfile="{githubworkspace}/linux/Makefile".format(githubworkspace=githubworkspace)
+
+# sourcedebversionfile="../mainline-kernel/.version"
+# kernelrevisionfile="../mainline-kernel/Makefile"
+
+
 with open(repoversionfile) as f:
 	values=f.readlines()
 	repobuildversion=values[0].strip('\n')
@@ -22,10 +27,12 @@ with open(kernelrevisionfile) as krevfile:
 			value=line.split('=')[1].replace(' ','')
 			config[key]=value
 		
-kernelversionfromsource='{}.{}.{}{}'.format(config['VERSION'],config["PATCHLEVEL"],config['SUBLEVEL'],config["EXTRAVERSION"])
-if repokernelrevision == kernelversionfromsource:
+kernelrevisionromsource='{}.{}.{}{}'.format(config['VERSION'],config["PATCHLEVEL"],config['SUBLEVEL'],config["EXTRAVERSION"])
+if repokernelrevision == kernelrevisionromsource:
 	with open(sourcedebversionfile,'w') as kversion:
 		kversion.write(repobuildversion)
 	print("SAME, copying debversion")
 else:
-	print('new kernel no version file needed')
+	with open(repoversionfile,'w') as repoversion:
+		repoversion.write('{}\n{}'.format('0',kernelrevisionromsource))	
+	print('new kernel, set debversion to 0')
